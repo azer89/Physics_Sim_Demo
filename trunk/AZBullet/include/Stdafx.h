9317ed87d7c142c34a3ef5127945e8af6807fc62
AzerDarkBlade@gmail.com <AzerDarkBlade@gmail.com@a9b9e392-9bec-d5d1-d17f-c5ce9f7909ae>
2011-10-16 07:24:57 +0000

    Configuring Files

---
+++ trunk/AZBullet/include/Stdafx.h
@@ -0,0 +1,23 @@
+
+#include <Ogre.h>
+#include <OgreMath.h>
+#include <OgreCamera.h>
+#include <OgreEntity.h>
+#include <OgreLogManager.h>
+#include <OgreRoot.h>
+#include <OgreViewport.h>
+#include <OgreSceneManager.h>
+#include <OgreRenderWindow.h>
+#include <OgreConfigFile.h>
+#include <OgreSimpleRenderable.h>
+
+#include <OISEvents.h>
+#include <OISInputManager.h>
+#include <OISKeyboard.h>
+#include <OISMouse.h>
+
+#include <SdkTrays.h>
+#include <SdkCameraMan.h>
+
+#include <Terrain/OgreTerrain.h>
+#include <Terrain/OgreTerrainGroup.h>
